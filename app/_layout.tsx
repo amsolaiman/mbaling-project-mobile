38a5cf0801e41598279d6d9531afa8406bfeb58c
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-25 13:26:11 +0800

    chore: import & setup fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,38 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+// @expo
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
+// hooks
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+// ----------------------------------------------------------------------
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+
+  const [loaded, error] = useFonts({
+    Metropolis100: require('../assets/fonts/Metropolis-100.ttf'),
+    Metropolis200: require('../assets/fonts/Metropolis-200.ttf'),
+    Metropolis300: require('../assets/fonts/Metropolis-300.ttf'),
+    Metropolis400: require('../assets/fonts/Metropolis-400.ttf'),
+    Metropolis500: require('../assets/fonts/Metropolis-500.ttf'),
+    Metropolis600: require('../assets/fonts/Metropolis-600.ttf'),
+    Metropolis700: require('../assets/fonts/Metropolis-700.ttf'),
+    Metropolis800: require('../assets/fonts/Metropolis-800.ttf'),
+    Metropolis900: require('../assets/fonts/Metropolis-900.ttf'),
   });
 
-  if (!loaded) {
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
     // Async font loading only occurs in development.
     return null;
   }
