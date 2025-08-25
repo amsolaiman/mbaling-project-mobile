b21ff7dbc39bc9715200d1c9d521eb62244f3b01
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-08-25 10:45:16 +0800

    chore(config): update eslint config (#7)

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,77 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
+
 const expoConfig = require('eslint-config-expo/flat');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+const reactHooksPlugin = require('eslint-plugin-react-hooks');
+const reactNativePlugin = require('eslint-plugin-react-native');
+const unusedImportsPlugin = require('eslint-plugin-unused-imports');
+
+// ----------------------------------------------------------------------
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-native': reactNativePlugin,
+      'unused-imports': unusedImportsPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      // GENERAL
+      'no-console': 'off',
+      'no-unused-vars': 'off',
+      'no-param-reassign': 'off',
+      'no-underscore-dangle': 'off',
+      'no-promise-executor-return': 'off',
+
+      // REACT
+      'react/display-name': 'off',
+      'react/no-children-prop': 'off',
+      'react/no-array-index-key': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react/require-default-props': 'off',
+      'react/jsx-props-no-spreading': 'off',
+
+      // IMPORTS
+      'import/no-unresolved': 'off',
+      'unused-imports/no-unused-imports': 'warn',
+
+      // TYPESCRIPT
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '**/ios/**', '**/android/**'],
   },
 ]);
