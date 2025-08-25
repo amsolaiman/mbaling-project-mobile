abdea55fe3b4f99207be22c93fb6b6a095674590
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-25 13:33:31 +0800

    feat: update themed-text w/ custom font

--- components/ThemedText.tsx
+++ components/ThemedText.tsx
@@ -1,60 +1,25 @@
-import { StyleSheet, Text, type TextProps } from 'react-native';
-
+import { Text, type TextProps } from 'react-native';
+// hooks
 import { useThemeColor } from '@/hooks/useThemeColor';
+// constants
+import Fonts from '@/constants/Fonts';
+
+// ----------------------------------------------------------------------
 
 export type ThemedTextProps = TextProps & {
   lightColor?: string;
   darkColor?: string;
-  type?: 'default' | 'title' | 'defaultSemiBold' | 'subtitle' | 'link';
+  font?: '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
 };
 
 export function ThemedText({
   style,
   lightColor,
   darkColor,
-  type = 'default',
+  font = '400',
   ...rest
 }: ThemedTextProps) {
   const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
 
-  return (
-    <Text
-      style={[
-        { color },
-        type === 'default' ? styles.default : undefined,
-        type === 'title' ? styles.title : undefined,
-        type === 'defaultSemiBold' ? styles.defaultSemiBold : undefined,
-        type === 'subtitle' ? styles.subtitle : undefined,
-        type === 'link' ? styles.link : undefined,
-        style,
-      ]}
-      {...rest}
-    />
-  );
+  return <Text style={[{ color, ...Fonts[font] }, style]} {...rest} />;
 }
-
-const styles = StyleSheet.create({
-  default: {
-    fontSize: 16,
-    lineHeight: 24,
-  },
-  defaultSemiBold: {
-    fontSize: 16,
-    lineHeight: 24,
-    fontWeight: '600',
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: 'bold',
-    lineHeight: 32,
-  },
-  subtitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  link: {
-    lineHeight: 30,
-    fontSize: 16,
-    color: '#0a7ea4',
-  },
-});
