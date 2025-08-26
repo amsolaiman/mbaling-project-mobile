85a1816388da667a8162c8abdf74f0eb204aa454
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-26 13:51:32 +0800

    feat: add react-hook-form & yup

---
+++ components/hook-form/form-provider.tsx
@@ -0,0 +1,12 @@
+import { FormProvider as Form, UseFormReturn } from 'react-hook-form';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  children: React.ReactNode;
+  methods: UseFormReturn<any>;
+};
+
+export default function FormProvider({ children, methods }: Props) {
+  return <Form {...methods}>{children}</Form>;
+}
