e4c6325aa681a85e26a6516d9997d3a4e224bef7
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-26 15:25:16 +0800

    chore: add themed keyboard-avoiding-view

--- components/themed-native/index.ts
+++ components/themed-native/index.ts
@@ -1,4 +1,5 @@
 export * from './use-theme-color';
 
+export { default as ThemedKeyboardAvoidingView } from './themed-keyboard-avoiding-view';
 export { default as ThemedText } from './themed-text';
 export { default as ThemedView } from './themed-view';
