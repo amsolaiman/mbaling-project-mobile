3a7955220e2e905abbfff94b1b745c5739fa86f9
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-27 12:51:38 +0800

    feat(auth): add auth context & hook

---
+++ auth/context/auth-consumer.tsx
@@ -0,0 +1,18 @@
+// components
+import SpinnerOverlay from '@/components/spinner-overlay';
+//
+import { AuthContext } from './auth-context';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export function AuthConsumer({ children }: Props) {
+  return (
+    <AuthContext.Consumer>
+      {(auth) => (auth.loading ? <SpinnerOverlay state={auth.loading} /> : children)}
+    </AuthContext.Consumer>
+  );
+}
