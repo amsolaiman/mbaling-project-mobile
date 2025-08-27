be3773b6f8d85c49e2649f83bb53cb7cff2811fb
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-08-27 13:42:06 +0800

    feat(auth): apply auth workflow on screens & layouts (#30)

--- app/(app)/(tabs)/settings.tsx
+++ app/(app)/(tabs)/settings.tsx
@@ -1,16 +1,31 @@
+import { useCallback } from 'react';
+import { Pressable } from 'react-native';
 // @expo
-import { Link } from 'expo-router';
+import { router } from 'expo-router';
+// auth
+import { useAuthContext } from '@/auth/hooks';
 // components
 import { ThemedText, ThemedView } from '@/components/themed-native';
 
 // ----------------------------------------------------------------------
 
 export default function SettingsScreen() {
+  const { logout } = useAuthContext();
+
+  const handleLogout = useCallback(async () => {
+    try {
+      await logout();
+      router.replace('/login');
+    } catch (error) {
+      console.error(error);
+    }
+  }, [logout]);
+
   return (
     <ThemedView className="flex-1 items-center justify-center">
-      <Link href="/login">
+      <Pressable onPress={handleLogout}>
         <ThemedText className="text-4xl">Settings</ThemedText>
-      </Link>
+      </Pressable>
     </ThemedView>
   );
 }
