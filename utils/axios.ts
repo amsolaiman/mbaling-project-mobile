4157696ec1abea0a56b41c142a34a1970b679248
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-27 12:36:46 +0800

    chore: create axios instance for auth

---
+++ utils/axios.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+// ----------------------------------------------------------------------
+
+const axiosInstance = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_HOST_API,
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+);
+
+export default axiosInstance;
+
+export const API_ENDPOINTS = {
+  auth: {
+    me: '/api/auth/me',
+    login: '/api/auth/login',
+  },
+};
