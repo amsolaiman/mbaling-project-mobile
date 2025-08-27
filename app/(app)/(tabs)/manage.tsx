aefac314e46c61a98c981b1a3fe4fb43c315393a
moizsolaiman <abdul.solaiman@accessgroup.net.au>
2025-08-27 13:59:20 +0800

    feat(auth): add role-based guard (#31)

--- app/(app)/(tabs)/manage.tsx
+++ app/(app)/(tabs)/manage.tsx
@@ -1,3 +1,5 @@
+// auth
+import { RoleBasedGuard } from '@/auth/guard';
 // components
 import { ThemedText, ThemedView } from '@/components/themed-native';
 
@@ -5,8 +7,10 @@ import { ThemedText, ThemedView } from '@/components/themed-native';
 
 export default function ManageScreen() {
   return (
-    <ThemedView className="flex-1 items-center justify-center">
-      <ThemedText className="text-4xl">Manage</ThemedText>
-    </ThemedView>
+    <RoleBasedGuard roles={['landlord']}>
+      <ThemedView className="flex-1 items-center justify-center">
+        <ThemedText className="text-4xl">Manage</ThemedText>
+      </ThemedView>
+    </RoleBasedGuard>
   );
 }
