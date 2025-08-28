7209d7beac4a3a471bc00869fe4bf1f664d6eeed
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-08-28 09:29:34 +0800

    feat(component): add post-card component

--- app/(app)/(tabs)/index.tsx
+++ app/(app)/(tabs)/index.tsx
@@ -1,12 +1,23 @@
 // components
-import { ThemedText, ThemedView } from '@/components/themed-native';
+import { ThemedView } from '@/components/themed-native';
+import { PostCard } from '@/screens/_components';
 
 // ----------------------------------------------------------------------
 
 export default function HomeScreen() {
   return (
     <ThemedView className="flex-1 items-center justify-center">
-      <ThemedText className="text-4xl">Welcome!</ThemedText>
+      <PostCard
+        item={{
+          id: 'a1b2c3d4-e5f6-7890-abcd-ef1234567890',
+          title: 'Available! Spacious ladies-only boarding room unit',
+          imageUrl: 'https://mbaling-project-api.vercel.app/assets/upload/upload_2.png',
+          userId: 'f5b2a8d5-9e7c-4bfa-bb99-88c1e9c458d3',
+          name: "Gania's Boarding House",
+          avatarUrl:
+            'https://mbaling-project-api.vercel.app/assets/avatar/demo/avatar_landlord.png',
+        }}
+      />
     </ThemedView>
   );
 }
--- assets/icons/index.ts
+++ assets/icons/index.ts
@@ -2,4 +2,5 @@ export * from './base';
 
 export { default as IconSpinner } from './spinner';
 
+export { default as IconMenuDots } from './menu-dots';
 export { default as IconQuestionMark } from './question-mark';
