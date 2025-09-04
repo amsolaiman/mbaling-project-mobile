7035e7fa2419207caeb8ed3ffd50f38bb5d1852d
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2025-09-04 14:27:06 +0800

    feat(component): rhf select, avatar & datepicker components

--- components/hook-form/index.ts
+++ components/hook-form/index.ts
@@ -1,3 +1,6 @@
 export { default } from './form-provider';
 
+export { default as RHFAvatar } from './rhf-avatar';
+export { default as RHFDatePicker } from './rhf-date-picker';
+export { default as RHFSelect } from './rhf-select';
 export { default as RHFTextField } from './rhf-textfield';
