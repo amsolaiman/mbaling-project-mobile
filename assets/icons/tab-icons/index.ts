ae523851e5c1acc989846743784a3ab9663cc8dd
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-08-04 07:25:34 +0800

    chore: add tab icon assets

---
+++ assets/icons/tab-icons/index.ts
@@ -0,0 +1,5 @@
+export { default as TabIconAccount } from './account';
+export { default as TabIconHome } from './home';
+export { default as TabIconManage } from './manage';
+export { default as TabIconSearch } from './search';
+export { default as TabIconSettings } from './settings';
