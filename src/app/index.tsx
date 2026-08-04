460b0ffbf3b1c212318fb0d159fc3a8f35cb93b1
Moiz--Access <abdul.solaiman@accessgroup.net.au>
2026-08-05 07:40:25 +0800

    feat: add & setup custom fonts (#15)
    * chore: import brand fonts
    
    * feat: setup custom fonts
    
    * test: display fonts for visual testing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,15 +2,21 @@ import { StyleSheet } from 'react-native';
 
 // components
 import { ThemedText, ThemedView } from '@/components/themed-native';
+// styles
+import { Fonts } from '@/styles';
 
 // ----------------------------------------------------------------------
 
 export default function HomeScreen() {
+  const fontWeights = Object.keys(Fonts) as unknown as (keyof typeof Fonts)[];
+
   return (
     <ThemedView style={styles.container}>
-      <ThemedText type="title" style={styles.title}>
-        Home
-      </ThemedText>
+      {fontWeights.map((weight) => (
+        <ThemedText key={weight} font={weight} style={styles.title}>
+          {weight}
+        </ThemedText>
+      ))}
     </ThemedView>
   );
 }
@@ -18,10 +24,14 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    gap: 8,
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
+    fontSize: 48,
+    lineHeight: 52,
     textAlign: 'center',
   },
 });
