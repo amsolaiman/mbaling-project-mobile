71c90995c3d8937ea3585143105a9317485dc52a
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-08-05 14:56:26 +0800

    feat: add spinner overlay to themed-view

--- src/components/themed-native/themed-view.tsx
+++ src/components/themed-native/themed-view.tsx
@@ -3,20 +3,29 @@ import { View, type ViewProps } from 'react-native';
 // hooks
 import { useTheme } from '@/hooks/use-theme';
 
+//
+import SpinnerOverlay from '../spinner-overlay';
+
 // ----------------------------------------------------------------------
 
 export type ThemedViewProps = ViewProps & {
-  lightColor?: string;
-  darkColor?: string;
+  loadingState?: boolean;
+  loadingCaption?: string;
 };
 
-export default function ThemedView({ style, ...otherProps }: ThemedViewProps) {
+export default function ThemedView({
+  style,
+  loadingState = false,
+  loadingCaption,
+  ...rest
+}: ThemedViewProps) {
   const theme = useTheme();
 
   return (
-    <View
-      style={[{ backgroundColor: theme.background }, style]}
-      {...otherProps}
-    />
+    <>
+      <SpinnerOverlay state={loadingState} caption={loadingCaption} />
+
+      <View style={[{ backgroundColor: theme.background }, style]} {...rest} />
+    </>
   );
 }
