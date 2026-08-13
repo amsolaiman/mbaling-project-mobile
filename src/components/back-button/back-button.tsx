dae93e1a272f7084c1a7b69d20c04bf6a4f74687
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-08-14 07:58:48 +0800

    feat: add back-button component

---
+++ src/components/back-button/back-button.tsx
@@ -0,0 +1,87 @@
+import { router } from 'expo-router';
+import { Pressable, StyleSheet } from 'react-native';
+
+// assets
+import { IconArrow } from '@/assets/icons';
+// hooks
+import { COMMON_COLORS } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Colors } from '@/styles';
+
+// ----------------------------------------------------------------------
+
+type Props = {
+  position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+  customFunc?: VoidFunction;
+};
+
+export default function BackButton({
+  position = 'top-left',
+  customFunc,
+}: Props) {
+  const scheme = useColorScheme();
+  const theme = scheme === 'unspecified' ? 'light' : scheme;
+
+  return (
+    <Pressable
+      onPress={!!customFunc ? customFunc : router.back}
+      style={[
+        styles.container,
+        {
+          backgroundColor:
+            scheme === 'light'
+              ? COMMON_COLORS.black[70]
+              : COMMON_COLORS.white[70],
+          ...getPosition(position),
+        },
+      ]}
+    >
+      <IconArrow
+        direction="left"
+        variant="outline"
+        size={24}
+        color={Colors[theme].text}
+      />
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    height: 42,
+    width: 42,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 50,
+    zIndex: 999,
+  },
+});
+
+const getPosition = (position: string) => {
+  switch (position) {
+    case 'top-right':
+      return {
+        top: 16,
+        right: 16,
+      };
+
+    case 'bottom-left':
+      return {
+        bottom: 16,
+        left: 16,
+      };
+
+    case 'bottom-right':
+      return {
+        bottom: 16,
+        right: 16,
+      };
+
+    default:
+      return {
+        top: 16,
+        left: 16,
+      };
+  }
+};
