558e11423c19a0c452ce15430b25482d52aa8a20
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-08-18 09:19:26 +0800

    refactor: update settings items rendering logic

--- src/screens/settings/main/settings-list.tsx
+++ src/screens/settings/main/settings-list.tsx
@@ -1,18 +1,26 @@
+import { Link } from 'expo-router';
+import {
+  openBrowserAsync,
+  WebBrowserPresentationStyle,
+} from 'expo-web-browser';
 import { SectionList, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 // assets
 import { IconArrow } from '@/assets/icons';
 // components
+import useCustomAlert from '@/components/custom-alert';
 import { ThemedText } from '@/components/themed-native';
 // constants
+import { SETTINGS_CONFIG } from '@/constants/settings';
 import { GREY_COLORS } from '@/constants/theme';
 // hooks
 import { useTheme } from '@/hooks/use-theme';
 // styles
 import { Fonts, Spacing } from '@/styles';
+// utils
+import { isExternalUrl } from '@/utils/url';
 
 //
-import { SettingsConfig } from './config-route';
 import SettingsLogout from './settings-logout';
 
 // ----------------------------------------------------------------------
@@ -20,33 +28,66 @@ import SettingsLogout from './settings-logout';
 export default function SettingsList() {
   const color = useTheme();
 
-  const data = SettingsConfig();
+  const { alert } = useCustomAlert();
 
   return (
     <SectionList
-      sections={data}
+      sections={SETTINGS_CONFIG}
       keyExtractor={(item) => item.label}
       contentContainerStyle={styles.container}
       //
       renderItem={({ item, index, section }) => {
         const isLast = index === section.data.length - 1;
+        const isExternal = isExternalUrl(item.href as string);
 
-        return (
-          <TouchableOpacity
-            onPress={item.onClick}
-            style={[styles.item, isLast && { marginBottom: Spacing.six }]}
-          >
+        const renderContent = (
+          <View style={[styles.item, isLast && { marginBottom: Spacing.six }]}>
             <ThemedText numberOfLines={1} style={styles.title}>
               {item.label}
             </ThemedText>
 
             <IconArrow
               variant="outline"
-              direction={item.isExternal ? 'right-up' : 'right'}
+              direction={isExternal ? 'right-up' : 'right'}
               size={24}
               color={color.text}
             />
-          </TouchableOpacity>
+          </View>
+        );
+
+        if (item.isNotLive) {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                alert({
+                  title: 'Oops!',
+                  message: 'This page is not yet live.',
+                })
+              }
+            >
+              {renderContent}
+            </TouchableOpacity>
+          );
+        }
+
+        return (
+          <Link
+            asChild
+            target={isExternal ? '_blank' : undefined}
+            href={item.href}
+            onPress={
+              isExternal
+                ? async (event) => {
+                    event.preventDefault();
+                    await openBrowserAsync(item.href as string, {
+                      presentationStyle: WebBrowserPresentationStyle.AUTOMATIC,
+                    });
+                  }
+                : undefined
+            }
+          >
+            <TouchableOpacity>{renderContent}</TouchableOpacity>
+          </Link>
         );
       }}
       renderSectionHeader={({ section }) => (
@@ -70,6 +111,7 @@ const styles = StyleSheet.create({
     color: GREY_COLORS[600],
   },
   item: {
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
   },
