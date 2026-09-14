5c4fda645717630aa7bb4872e89b7b8c664c1e43
Abdul Moiz Solaiman <abdul.solaiman@accessgroup.net.au>
2026-09-14 08:33:41 +0800

    fix: remove excessive buttom padding of account settings page

--- src/screens/settings/account/account-footer.tsx
+++ src/screens/settings/account/account-footer.tsx
@@ -5,7 +5,7 @@ import InfoBanner from '@/components/info-banner';
 // constants
 import { GREY_COLORS } from '@/constants/theme';
 // styles
-import { BOTTOM_TAB_BAR_INSET, Spacing } from '@/styles';
+import { Spacing } from '@/styles';
 
 // ----------------------------------------------------------------------
 
@@ -22,8 +22,7 @@ export default function SettingsAccountFooter() {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: Spacing.five,
-    paddingBottom: BOTTOM_TAB_BAR_INSET + Spacing.five,
+    paddingVertical: Spacing.five,
     paddingHorizontal: Spacing.four,
     borderTopWidth: 1.5,
     borderColor: GREY_COLORS[300],
